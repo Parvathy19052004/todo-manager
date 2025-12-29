602d43f3f74cba2656dbd39000ee585fce4d4527
parvathy <nairparvathy130@gmail.com>
2025-12-29 11:15:33 +0530

    Add add_task feature to todo manager

--- todo.py
+++ todo.py
@@ -2,11 +2,22 @@ tasks = []
 
 def add_task(task):
     tasks.append(task)
-    print("Task added")
+    #print("Task added")
 
 def view_tasks():
     for i, task in enumerate(tasks):
-        print(i+1, task)
+        #print(i+1, task)
+        
 def add_task(tasks, task):
     tasks.append({"task": task, "done": False})
+    
     print("Task added successfully")
+    
+# Main branch change
+
+# Feature change 1
+# Feature change 2
+# Another main change
+# temp1
+# temp2
+# temp3
